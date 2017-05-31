bb797c3087d85799f0d8dfb4509dc3ef0e5cedfd
akmal <akmal@brighter.com>
2017-05-31 12:17:24 -0700

    convert MarkUpContainer to a class

--- js/MarkUpContainer.js
+++ js/MarkUpContainer.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React,{Component} from 'react'
 import MarkUpPreview from './MarkUpPreview'
 import MarkUpEdit from './MarkUpEdit'
 
-var MarkUpContainer = React.createClass({
-    getInitialState : function() {
-        return {
-			newMarkUp: ''
-        }
-    },
 
-    render: function(){
-        return (
+class MarkUpContainer extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+			newMarkUp: ''
+		}
+
+		this.handleChange = this.handleChange.bind(this);
+    }
+
+	handleChange(evt){
+		this.setState({
+			newMarkUp: evt.target.value
+		});
+    }
+
+    render(){
+		return (
             <div className="container-fluid">
                 <div className="row">
                     <h1 className="text-center">
@@ -21,13 +31,8 @@ var MarkUpContainer = React.createClass({
                     <MarkUpPreview markUp={this.state.newMarkUp}/>
                 </div>
             </div>
-        )
-    },
-    handleChange: function (evt) {
-        this.setState({
-            newMarkUp: evt.target.value
-        });
+		)
     }
-})
+}
 
 export default MarkUpContainer
